ffbf1c3184c8d2ee3053017cf871303a68355f9c
Avigail Margolin <avigail.margolin@gmail.com>
2020-12-05 13:41:31 -0800

    changes to the proxy repository

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,6 +4,12 @@ import ReactDOM from 'react-dom';
 import App from './components/app.jsx';
 import 'semantic-ui-css/semantic.min.css';
 
+var isbn = window.location.href.split('/').reverse()[0];
+console.log("BROWSER URL IS:", isbn);
+
 ReactDOM.render(
-  <App isbn13={'9781524763169'}/>,
-  document.getElementById('app'));
+  <App isbn13={isbn}/>,
+  document.getElementById('product-details'));
+
+
+
